a9327ca19c357da6419d81d6df32e25e9ac0dba1
Yigit Ocak <yigitocak@Yigits-MBP.lan>
2022-11-27 22:25:56 -0600

    Routes.js is created, now able to POST

---
+++ api.js
@@ -0,0 +1,8 @@
+const { application } = require('express');
+const express = require('express');
+const path = require('path');
+
+module.exports = app => {
+   
+}
+
--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-const { application } = require('express');
 const express = require('express');
 const path = require('path');
+const api = require('./api');
 
 const app = express();
 const PORT = 3001;
 
-const notesHTMLPath = path.join(__dirname, 'public/notes.html'); //joins the directory name with the route
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); //access the files inside the 'public' folder
-app.get('/notes', (req, res) => res.sendFile(notesHTMLPath)); //sends the notes.html to the server
 
-
-const middleware = (req, res, next) => {
-  // ANSI escape code that instructs the terminal to print in yellow
-  const yellow = '\x1b[33m%s\x1b[0m';
-
-  // Log out the request type and resource
-  console.log(yellow, `${req.method} request to ${req.path}`);
-
-  // Built-in express method to call the next middleware in the stack.
-  next();
-};
-
-app.use(middleware);
+require('./public/routes.js')(app);
+// require('./api')(app);
 
 app.listen(PORT, () =>
-  console.log(`Listening for requests on port http://localhost:${PORT}/ 🏎️`)
+  console.log(`Listening for requests on port http://localhost:${PORT}/`)
 );
